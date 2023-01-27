095a7c2fdb3deb3c51b052c18a8ec7ecc9cf74bb
Haider Al-Barakat <hal-barakat@unomaha.edu>
2023-01-27 11:28:07 -0600

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Haider Al-Barakat
+ *        hal-barakat@unomaha.edu
+ * Date: 2023/01/27
+ *
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("My major is Computer Engineering!\n");
+  printf("Haider Al-Barakat!\n");
+
+
+  return 0;
+}
